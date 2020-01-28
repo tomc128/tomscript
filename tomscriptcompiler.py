7918f47d2df7c53e775ac8bbab771ef3e7f61b9a
Airodene <nath.thorp@gmail.com>
2020-01-28 16:51:58 +0000

    Prep for goto

--- tomscriptcompiler.py
+++ tomscriptcompiler.py
@@ -6,6 +6,7 @@ optionCompileComments = False
 optionCompileExecutable = False
 optionCompileDebug = False
 startOffset = 0
+lineCount = 0
 
 def loadSettings():
     global sourceFile, outputFile, optionCompileComments, optionCompileExecutable, optionCompileDebug, commentCount, startOffset
